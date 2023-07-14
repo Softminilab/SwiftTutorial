b81d9a153452c1d7ad4f20067d2d38c3626d8eb0
Softminilab <karepbq@gmail.com>
2023-07-14 15:28:54 +0800

    Added: Test Dynamic library sizes

--- SwiftTutorial/SwiftTutorialApp.swift
+++ SwiftTutorial/SwiftTutorialApp.swift
@@ -9,9 +9,39 @@ import SwiftUI
 
 @main
 struct SwiftTutorialApp: App {
+    init() {
+        getDynamicLibrarySizes()
+    }
+    
     var body: some Scene {
         WindowGroup {
             TextSample()
+        }
+    }
+}
+
+extension SwiftTutorialApp {
+    func getDynamicLibrarySizes() {
+        let fileURL = Bundle.main.url(forResource: "dynamic_library_sizes", withExtension: "txt")
+        guard let filePath = fileURL?.path else {
+            print("Failed to find dynamic_library_sizes.txt file.")
+            return
+        }
+        
+        do {
+            let sizesString = try String(contentsOfFile: filePath, encoding: .utf8)
+            let sizes = sizesString.components(separatedBy: .newlines)
+            
+            for sizeString in sizes {
+                let components = sizeString.components(separatedBy: " ")
+                if components.count == 2 {
+                    let libraryName = components[0]
+                    let librarySize = components[1]
+                    print("Dynamic Library: \(libraryName), Size: \(librarySize) bytes")
+                }
+            }
+        } catch {
+            print("Failed to read dynamic_library_sizes.txt file: \(error)")
         }
     }
 }
